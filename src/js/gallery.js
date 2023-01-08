e2989fc83d874b3744fb5d7d8bd9619502489893
Sandeep Shrestha <sandeepshrestha288@gmail.com>
2023-01-08 16:33:44 +0545

    - gallery updated

---
+++ src/js/gallery.js
@@ -0,0 +1,8 @@
+// not for use
+for(i=1;i<=2;i++){
+    document.writeln('<div class="rounded-xl hover:scale-110 duration-500 shadow-lg shadow-gray-500">');
+    document.writeln('  <a data-fancybox="gallery" href="images/gallery/img ('+i+').jpg">');
+    document.writeln('      <img src="images/gallery/img ('+i+').jpg" class="rounded-xl aspect-[4/3] object-center" alt="img ('+i+')">');
+    document.writeln('  </a>');
+    document.writeln('</div');
+}
